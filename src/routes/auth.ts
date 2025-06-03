ce5f71514b6cd7cee41389719f6a1c31bbb577be
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-06-04 03:19:27 +0700

    refactor : ensure auth and location route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,6 +12,11 @@ const prisma = new PrismaClient();
 
 router.use(express.json());
 
+type CustomError = {
+  status: number;
+  message: string;
+};
+
 type CustomRequest = Request & { userId: string };
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
@@ -48,208 +53,153 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 router.post("/register", async (req: Request, res: Response) => {
-  const {
-    username = null,
-    email = null,
-    password = null,
-    phoneNumber = null,
-  } = req.body;
-  if (!username) {
-    res.status(400).json({
-      message: "User tidak disertakan",
-      status: false,
-    });
-  }
+  try {
+    const {
+      username = null,
+      email = null,
+      password = null,
+      phoneNumber = null,
+    } = req.body;
 
-  if (!email) {
-    console.log("ga ada email");
-    res.status(400).json({
-      message: "Email tidak disertakan",
-      status: false,
-    });
-  }
-  if (!password) {
-    res.status(400).json({
-      message: "Password tidak disertakan",
-      status: false,
-    });
-  }
+    if (!username) {
+      throw {
+        status: 400,
+        message: "Username tidak disediakan. Masukkan Username",
+      };
+    }
 
-  const findEmail = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
+    if (!email) {
+      throw { status: 400, message: "Email tidak disediakan. Masukkan Email" };
+    }
+    if (!password) {
+      throw {
+        status: 400,
+        message: "Password tidak disediakan. Masukkan Password",
+      };
+    }
 
-  if (findEmail) {
-    res.status(400).json({
+    const findEmail = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (findEmail) {
+      throw {
+        status: 400,
+        message:
+          "Email yang dimasukkan sudah pernah digunakan, gunakan email lain",
+      };
+    } else {
+      const createUser = await prisma.user.create({
+        data: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          ...(phoneNumber && { phoneNumber: phoneNumber }),
+        },
+      });
+      res.status(201).json({
+        success: true,
+        message: "Akunmu berhasil terdaftar di FindSoed",
+        data: {
+          id: createUser.id,
+          username: createUser.username,
+          email: createUser.email,
+        },
+      });
+    }
+  } catch (e) {
+    res.status((e as CustomError).status).json({
       success: false,
-      message: "Email yang kamu masukkan, telah digunakan",
-    });
-  } else {
-    const createUser = await prisma.user.create({
-      data: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        ...(phoneNumber && {phoneNumber : phoneNumber}),
-      },
-    });
-
-    res.status(201).json({
-      success: true,
-      message: {
-        id: createUser.id,
-        username: createUser.username,
-        email: createUser.email,
-      },
+      message: (e as CustomError).message,
     });
   }
 });
 
 router.post("/login", async (req: Request, res: Response) => {
-  const { email = null, password = null } = req.body;
-  if (!email) {
-    res.status(404).json({
-      success: false,
-      message: "Email tidak disertakan",
-    });
-  }
-
-  if (!password) {
-    res.status(404).json({
-      success: false,
-      message: "Password tidak disertakan",
-    });
-  }
-  const findEmail = await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
-  });
-
-  const findProfile = await prisma.profile.findUnique({
-    where: {
-      userId: findEmail?.id,
-    },
-  });
-  if (!findEmail) {
-    res.status(404).json({
-      success: false,
-      message: "Email yang kamu masukkan tidak terdaftar",
-    });
-  } else {
-    if (findEmail.password === password) {
-      const token = jwt.sign(
-        { userId: findEmail.id },
-        process.env.JWT_SECRET as string,
-        { expiresIn: "30m" },
-      );
-      res.status(200).json({
-        success: true,
-        message: "Selamat anda telah berhasil log in",
-        data: {
-          token: token,
-          userId: findEmail.id,
-          username: findEmail.username,
-          email: findEmail.email,
-          phoneNumber: findEmail.phoneNumber ? findEmail.phoneNumber : null,
-          imageUrl: findProfile ? findProfile.imageUrl : null,
-        },
-      });
-    } else {
-      res.status(400).json({
-        success: false,
-        message: "Password tidak sesuai",
-      });
+  try {
+    const { email = null, password = null } = req.body;
+    if (!email) {
+      throw { status: 400, message: "Email belum dimasukkan" };
     }
+
+    if (!password) {
+      throw { status: 400, message: "Password belum dimasukkan" };
+    }
+
+    const findEmail = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (findEmail) {
+      if (findEmail.password === password) {
+        const findProfile = await prisma.profile.findUnique({
+          where: {
+            userId: findEmail.id,
+          },
+        });
+        const token = jwt.sign(
+          { userId: findEmail.id },
+          process.env.JWT_SECRET as string,
+          { expiresIn: "1h" },
+        );
+        res.status(200).json({
+          success: true,
+          message: "Login Berhasil",
+          data: {
+            token: token,
+            userId: findEmail.id,
+            username: findEmail.username,
+            email: findEmail.email,
+            phoneNumber: findEmail.phoneNumber ? findEmail.phoneNumber : null,
+            imageUrl: findProfile ? findProfile.imageUrl : null,
+          },
+        });
+      } else {
+        throw { status: 401, message: "Password yang kamu masukkan salah" };
+      }
+    } else {
+      throw { status: 404, message: "Email yang kamu masukkan tidak tersedia" };
+    }
+  } catch (e) {
+    console.log("isi e : ", e);
+    res.status((e as CustomError).status).json({
+      success: false,
+      message: (e as CustomError).message,
+    });
   }
 });
 
-router.get(
-  "/find",
-  verifyToken,
-  async (req: Request, res: Response) => {
-    try {
-      //@ts-ignore
-      console.log("id here: ", req.userId);
-      const findUser = await prisma.user.findUnique({
-        where: {
-          //@ts-ignore
-          id: req.userId,
-        },
-      });
-
-      if (findUser) {
-        const findProfile = await prisma.profile.findUnique({
-          where: {
-            userId: findUser.id,
-          },
-
-        
-        });
-
-        res.status(200).json({
-          success: true,
-          message: "Berhasil mendapat informasi pengguna",
-          data: {
-            userId: findUser?.id,
-            username: findUser?.username,
-            email: findUser?.email,
-            phoneNumber: findUser?.phoneNumber,
-            imageUrl: findProfile?.imageUrl ? findProfile?.imageUrl : null,
-          },
-        });
-        
-      }
-    } catch (err) {
-      res.status(404).json({
-        success: false,
-        message: err,
-      });
-    }
-  },
-);
-
-router.patch("/edit", verifyToken, async (req: Request, res: Response) => {
-  const { password = null } = req.body;
+router.get("/find", verifyToken, async (req: Request, res: Response) => {
   try {
-    if (password) {
-      const newUserPassword = await prisma.user.update({
+    //@ts-ignore
+    console.log("id here: ", req.userId);
+    const findUser = await prisma.user.findUnique({
+      where: {
+        //@ts-ignore
+        id: req.userId,
+      },
+    });
+
+    if (findUser) {
+      const findProfile = await prisma.profile.findUnique({
         where: {
-          //@ts-ignore
-          id: req.userId,
-        },
-        data: {
-          password: password,
+          userId: findUser.id,
         },
       });
-    }
-    let newImage;
-    if (req.files) {
-      const findImage = await prisma.profile.findUnique({
-        where: {
-          //@ts-ignore
-          userId: req.userId,
-        },
-      });
-      if (findImage) {
-        newImage = await prisma.profile.update({
-          where: {
-            //@ts-ignore
-            userId: req.userId,
-          },
-          data: {
-            //@ts-ignore
-            imageUrl: (req.files as Express.Multer.File).filename,
-          },
-        });
-      }
+
       res.status(200).json({
         success: true,
-        message: "Profil berhasil diubah",
+        message: "Berhasil mendapat informasi pengguna",
         data: {
-          user: [],
+          userId: findUser?.id,
+          username: findUser?.username,
+          email: findUser?.email,
+          phoneNumber: findUser?.phoneNumber,
+          imageUrl: findProfile?.imageUrl ? findProfile?.imageUrl : null,
         },
       });
     }
@@ -335,7 +285,7 @@ router.patch(
       }
     } catch (e) {
       console.log(e);
-      res.status(400).json({
+      res.status(500).json({
         success: false,
         message: e,
       });
@@ -346,7 +296,7 @@ router.patch(
 router.patch("/editdata", verifyToken, async (req: Request, res: Response) => {
   try {
     const { userPassword = null, userPhoneNumber = null } = req.body;
-    console.log(userPhoneNumber)
+    console.log(userPhoneNumber);
     const findUser = await prisma.user.findUnique({
       where: {
         //@ts-ignore
@@ -359,7 +309,7 @@ router.patch("/editdata", verifyToken, async (req: Request, res: Response) => {
           id: findUser.id,
         },
         data: {
-          password:  userPassword ? userPassword : findUser.password,
+          password: userPassword ? userPassword : findUser.password,
           phoneNumber: userPhoneNumber ? userPhoneNumber : findUser.phoneNumber,
         },
       });
@@ -386,8 +336,7 @@ router.patch("/editdata", verifyToken, async (req: Request, res: Response) => {
       }
     }
   } catch (e) {
-    console.log("e : ", e)
-    res.status(400).json({
+    res.status(500).json({
       success: false,
       message: e,
     });
